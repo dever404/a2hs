87b0055758972a5618a09dc88794c0b3469e6d19
errafaiy <abdo.errafaiy@gmail.com>
2019-10-17 18:31:46 +0100

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,1 @@
-console.log("service js"),"serviceWorker"in navigator&&(console.log("serviceWorker load"),navigator.serviceWorker.register("/sw.js").then(function(e){console.log("Service Worker registered ",e)}).catch(function(e){console.error("Service Worker fial ",e)}));
+console.log("service js"),"serviceWorker"in navigator&&(console.log("serviceWorker load"),navigator.serviceWorker.register("/a2hs/sw.js").then(function(e){console.log("Service Worker registered ",e)}).catch(function(e){console.error("Service Worker fial ",e)}));
