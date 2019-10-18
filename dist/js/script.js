971a05f6232d9d91e61ed63ea6fb0569598a5f57
errafaiy <abdo.errafaiy@gmail.com>
2019-10-18 10:51:35 +0100

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
-console.log('service js');
+alert('service js');
 
 if ('serviceWorker' in navigator) {
-    console.log('serviceWorker load');
+    alert('serviceWorker load');
     navigator.serviceWorker.register('/a2hs/sw.js')
     .then(function (reg) {
         console.log("Service Worker registered ",reg); // console.log(reg);
@@ -9,35 +9,3 @@ if ('serviceWorker' in navigator) {
         console.error("Service Worker fial ",err); // console.error(err);
     });
 }  
-
-// Code to handle install prompt on desktop
-
-var deferredPrompt;
-var addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
-console.log('btn :' + addBtn);
-window.addEventListener('beforeinstallprompt', function (e) {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault(); // Stash the event so it can be triggered later.
-
-  deferredPrompt = e; // Update UI to notify the user they can add to home screen
-  ///showInstallPromotion();
-
-  addBtn.style.display = 'block';
-  addBtn.addEventListener('click', function (e) {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none'; // Show the prompt
-
-    deferredPrompt.prompt(); // Wait for the user to respond to the prompt
-
-    deferredPrompt.userChoice.then(function (choiceResult) {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-
-      deferredPrompt = null;
-    });
-  });
-});
