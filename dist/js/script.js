8e3ab68a8863667ee585bbb764b8fc08f8e4cd6d
errafaiy <abdo.errafaiy@gmail.com>
2019-10-18 12:11:13 +0100

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,7 @@
 alert('service js');
 
-//if ('serviceWorker' in navigator) {
-if(window.isSecureContext){
+if ('serviceWorker' in navigator) {
+//if(window.isSecureContext){
     alert('serviceWorker load');
     navigator.serviceWorker.register('/a2hs/sw.js')
     .then(function (reg) {
@@ -12,4 +12,22 @@ if(window.isSecureContext){
 }  
 else {
      alert('Your browser does not support the Service-Worker!');
+}
+
+// Detects if device is on iOS 
+var isIos = function isIos() {
+  var userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test(userAgent);
+}; // Detects if device is in standalone mode
+
+
+var isInStandaloneMode = function isInStandaloneMode() {
+  return 'standalone' in window.navigator && window.navigator.standalone;
+}; // Checks if should display install popup notification:
+
+
+if (isIos() && !isInStandaloneMode()) {
+  (void 0).setState({
+    showInstallMessage: true
+  });
 }
